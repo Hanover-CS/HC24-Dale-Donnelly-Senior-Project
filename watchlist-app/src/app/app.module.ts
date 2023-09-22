05fc8c33a189135aae3439176f6e9bc02a6354ab
Dale Donnelly <d.ded472@gmail.com>
2023-09-22 19:13:10 -0400

    added movie service to display data from json file ref Display movie list on main page #15

--- watchlist-app/src/app/app.module.ts
+++ watchlist-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MovieComponent } from './components/movie/movie.component';
@@ -12,7 +13,8 @@ import { MovieListComponent } from './components/movie-list/movie-list.component
     MovieListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- watchlist-app/src/app/services/movie.service.ts
+++ watchlist-app/src/app/services/movie.service.ts
@@ -1,23 +1,40 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { MovieData } from 'src/lib/types/MovieData';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  movieJson: any;
+  movieData: MovieData[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getMovieData() {
-    return [
-      {
-        id: 0,
-        title: "Movie",
-        overview: "Description",
-        imagePath: "img",
-        releaseDate: "9/22/2023",
-        genreIds: [0, 1]
+  private getJSON() {
+    return this.http.get<any>('../../assets/data/movies.json');
+  }
+
+  private populateMovieData() {
+    this.getJSON().subscribe(data => {
+      this.movieJson = data;
+      for (const m of this.movieJson.results) {
+        this.movieData.push({
+          id: m.id,
+          title: m.title,
+          overview: m.overview,
+          imagePath: m.poster_path,
+          releaseDate: m.release_date,
+          genreIds: m.genre_ids
+        })
       }
-    ];
+    })
+  }
+
+  getMovieData(): MovieData[] {
+    this.populateMovieData()
+    return this.movieData;
   }
 
 }
