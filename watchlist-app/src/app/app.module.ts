6c40d48a9940ca8c93d87c2bf8e12df309e85ff9
Dale Donnelly <d.ded472@gmail.com>
2023-09-26 13:24:03 -0400

    initial commit for movie-details component ref #16

--- watchlist-app/src/app/app.module.ts
+++ watchlist-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './components/movie/movie.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MovieComponent,
-    MovieListComponent
+    MovieListComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
