b52d3d77aae2a578a834b7b0fdcdb03d022facb1
Dale Donnelly <d.ded472@gmail.com>
2023-09-28 16:33:52 -0400

    removed old code from movie service

--- watchlist-app/src/app/services/movie.service.ts
+++ watchlist-app/src/app/services/movie.service.ts
@@ -52,43 +52,4 @@ export class MovieService {
     this.singleMovies.set(id, movieMatch)
     return movieMatch
    }
-
-  /* mapJsonToMovieData() {
-    let movieArr: MovieData[] = []
-    this.movies = of(movieArr)
-    console.log(this.movies)
-    console.log("mapJsonToMovieData() finished running")
-   } */
-
-
-  /* private getJSON() {
-    return this.http.get<any>('assets/data/movies.json');
-  }
-
-  private retrieveMovies(): MovieData[] {
-    const movies: MovieData[] = []
-    this.getJSON().subscribe(data => {
-      this.movieJson = data;
-      for (const m of this.movieJson.results) {
-        movies.push({
-          id: m.id,
-          title: m.title,
-          overview: m.overview,
-          imagePath: `https://image.tmdb.org/t/p/w220_and_h330_face${m.poster_path}`,
-          releaseDate: m.release_date,
-          genreIds: m.genre_ids
-        })
-      }
-    })
-    return movies;
-  }
-
-  getAllMovieData(): MovieData[] {
-    return this.movieData;
-  }
-
-  getMovieById(id: Number): MovieData {
-    const movie = this.movieData.filter((m) => m.id === id);
-    return movie[0];
-  } */
 }
