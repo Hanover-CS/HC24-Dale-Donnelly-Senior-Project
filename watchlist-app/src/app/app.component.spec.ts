71abe8b98e7f96c35cdd49bce59add4d42d51f76
Dale Donnelly <d.ded472@gmail.com>
2023-10-16 16:18:27 -0400

    app component uses router testing module ref #23

--- watchlist-app/src/app/app.component.spec.ts
+++ watchlist-app/src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
@@ -9,7 +9,7 @@ describe('AppComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [RouterModule] // Include your component in the testing module
+      imports: [RouterTestingModule] // Include your component in the testing module
     });
 
     fixture = TestBed.createComponent(AppComponent);
