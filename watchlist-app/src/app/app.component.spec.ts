c3ee84baeeb41d8a8152cf7cd61529d4e935133d
Dale Donnelly <d.ded472@gmail.com>
2023-10-16 16:03:41 -0400

    added imports to app component test ref #23

--- watchlist-app/src/app/app.component.spec.ts
+++ watchlist-app/src/app/app.component.spec.ts
@@ -1,17 +1,22 @@
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
-describe('MyComponent', () => {
+describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AppComponent], // Include your component in the testing module
+      declarations: [AppComponent],
+      imports: [RouterModule] // Include your component in the testing module
     });
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
   });
-});
 
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
