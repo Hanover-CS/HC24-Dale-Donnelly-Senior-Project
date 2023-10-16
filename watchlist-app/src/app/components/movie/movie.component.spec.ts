e61b9182827b043e61b517086c70b6c4a064848e
Dale Donnelly <d.ded472@gmail.com>
2023-10-16 16:15:30 -0400

    added routing and data to movie component test ref #23

--- watchlist-app/src/app/components/movie/movie.component.spec.ts
+++ watchlist-app/src/app/components/movie/movie.component.spec.ts
@@ -1,17 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieComponent } from './movie.component';
+import { MovieData } from 'src/lib/types/MovieData';
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('MovieComponent', () => {
   let component: MovieComponent;
   let fixture: ComponentFixture<MovieComponent>;
 
+  const movie: MovieData = {
+    id: 0,
+    title: 'title',
+    overview: 'desc',
+    imagePath: 'img',
+    releaseDate: '2023-10-16',
+    genreIds: [0]
+  }
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MovieComponent]
+      declarations: [MovieComponent],
+      imports: [RouterTestingModule]
     });
+
     fixture = TestBed.createComponent(MovieComponent);
     component = fixture.componentInstance;
+    component.movie = movie;
     fixture.detectChanges();
   });
 
