cc94ef681a01db665906398b70b202ddd444fe19
Dale Donnelly <d.ded472@gmail.com>
2023-10-17 14:59:30 -0400

    added unit tests for movie components

--- watchlist-app/src/app/components/movie/movie.component.spec.ts
+++ watchlist-app/src/app/components/movie/movie.component.spec.ts
@@ -12,7 +12,7 @@ describe('MovieComponent', () => {
     id: 0,
     title: 'title',
     overview: 'desc',
-    imagePath: 'img',
+    imagePath: 'http://example.com/',
     releaseDate: '2023-10-16',
     genreIds: [0]
   }
@@ -31,5 +31,23 @@ describe('MovieComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create movie image element', () => {
+    expect(document.getElementById("movieImg")).toBeTruthy();
+  })
+
+  it('should display movie image', () => {
+    const img: HTMLImageElement = document.getElementById("movieImg") as HTMLImageElement
+    expect(img.src).toEqual(movie.imagePath);
+  });
+
+  it('should create movie title element', () => {
+    expect(document.getElementById("movieTitle")).toBeTruthy();
+  })
+
+  it('should display movie title', () => {
+    const title = document.getElementById("movieTitle")?.textContent
+    expect(title).toEqual(movie.title);
   });
 });
