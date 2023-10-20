724f891c553bb909a6a93d63768cc4f4ace7ced8
Dale Donnelly <d.ded472@gmail.com>
2023-10-20 17:07:03 -0400

    switched to normal http module in test ref #28

--- watchlist-app/src/app/services/movie.service.spec.ts
+++ watchlist-app/src/app/services/movie.service.spec.ts
@@ -2,15 +2,16 @@ import { TestBed } from '@angular/core/testing';
 
 import { MovieService } from './movie.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestingUrl } from 'src/lib/types/urls';
+import { ImageUrl, TestingUrl } from 'src/lib/types/urls';
 import { MovieData } from 'src/lib/types/MovieData';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('MovieService', () => {
   let service: MovieService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientModule]
     });
     service = TestBed.inject(MovieService);
   });
@@ -23,15 +24,32 @@ describe('MovieService', () => {
     service.url = TestingUrl
     const m = service.getAllMovies()
     expect(m).toBeTruthy();
-  })
+  });
 
-  xit('should get all movies', (done) => {
-    const testMovies: MovieData[] = []
+  it('should get all movies as movie data', (done) => {
+    const testMovies: MovieData[] = [
+      {
+        id: 0, 
+        title: 'Title', 
+        imagePath: ImageUrl+'/kdPMUMJzyYAc4roD52qavX0nLIC.jpg', 
+        overview: 'Description', 
+        releaseDate: '2023-10-19', 
+        genreIds: [27, 53]
+      },
+      {
+        id: 1,
+        title: 'Title 1',
+        imagePath: ImageUrl+'/fiVW06jE7z9YnO4trhaMEdclSiC.jpg',
+        overview: 'Description 1',
+        releaseDate: '2023-10-19',
+        genreIds: [28, 80, 53]
+      }
+    ]
     service.url = TestingUrl
     const m = service.getAllMovies()
     m.subscribe(results => {
       expect(results).toEqual(testMovies)
       done()
     })
-  })
+  });
 });
