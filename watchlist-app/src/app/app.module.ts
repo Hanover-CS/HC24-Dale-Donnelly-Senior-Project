55a17a1705e319871330a4c3f15ae2f9cdef4253
Dale Donnelly <d.ded472@gmail.com>
2023-10-25 11:58:45 -0400

    appModule does not use emulators

--- watchlist-app/src/app/app.module.ts
+++ watchlist-app/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore, Firestore } from '@angular/fire/firestore';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 import { AppComponent } from './app.component';
 import { MovieComponent } from './components/movie/movie.component';
 import { MovieListComponent } from './components/movie-list/movie-list.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { AppRoutingModule } from './app-routing.module';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 import { StarRatingModule } from 'angular-star-rating';
-import { connectFirestoreEmulator, initializeFirestore } from 'firebase/firestore';
-import { getApp } from 'firebase/app';
 
 @NgModule({
   declarations: [
@@ -27,16 +25,7 @@ import { getApp } from 'firebase/app';
     AppRoutingModule,
     StarRatingModule.forRoot(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => {
-      let firestore: Firestore
-      if (environment.useEmulators) {
-        firestore = initializeFirestore(getApp(), {})
-        connectFirestoreEmulator(firestore, 'localhost', 8080)
-      } else {
-        firestore = getFirestore()
-      }
-      return firestore
-    })
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
