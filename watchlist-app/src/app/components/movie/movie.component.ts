9d50b784b7a9ec36029c4d9568cda153692186d9
Dale Donnelly <d.ded472@gmail.com>
2023-11-02 22:06:56 -0400

    Added docs for MovieDetails component ref #33

--- watchlist-app/src/app/components/movie/movie.component.ts
+++ watchlist-app/src/app/components/movie/movie.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { MovieData } from 'src/lib/types/MovieData';
-
+/**
+ * Generates a movie component, generated from the provided MovieData parameter.
+ * Movie component consists of a poster and a title, which are presented in
+ * the homepage lists.
+ */
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
