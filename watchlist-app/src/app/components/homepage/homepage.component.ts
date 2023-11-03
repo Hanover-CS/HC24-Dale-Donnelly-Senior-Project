8ec426ac788af5bc5d6fefb51145aebbe6719847
Dale Donnelly <d.ded472@gmail.com>
2023-11-02 22:06:42 -0400

    added docs for homepage component ref #33

--- watchlist-app/src/app/components/homepage/homepage.component.ts
+++ watchlist-app/src/app/components/homepage/homepage.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { GenreIds } from 'src/lib/types/Genre';
-
+/**
+ * Homepage Component populates the landing page with movie lists.
+ * Iterates over the list of GenreIds to create a list of movies for each one.
+ */
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
