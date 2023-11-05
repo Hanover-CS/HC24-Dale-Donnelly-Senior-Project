46abfcfba495584e4f1404eb5686b623367094d4
Dale Donnelly <d.ded472@gmail.com>
2023-11-05 12:54:39 -0500

    added docs for MovieDetails component ref #33

--- watchlist-app/src/app/components/movie-details/movie-details.component.ts
+++ watchlist-app/src/app/components/movie-details/movie-details.component.ts
@@ -8,6 +8,13 @@ import { MovieData } from 'src/lib/types/MovieData';
 import { Review } from 'src/lib/types/Review';
 import { GenreIdToGenre } from 'src/lib/types/Genre';
 
+/**
+ * Generates a MovieDetails component which displays title info and facilitates user interaction for reviewing the titles.
+ * 
+ * Displayed info includes the title, overview, poster, and genres.
+ * 
+ * Component page route is generated from the title's ID, which is then used to retrieve title data.
+ */
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
@@ -21,6 +28,11 @@ export class MovieDetailsComponent implements OnInit {
   reviews !: Observable<Review[]>;
   genreIdToGenre = GenreIdToGenre
 
+  /**
+   * Constructs a Review interface object and calls Firestore to post the review to the database.
+   * @param rating Number, score of the review to be posted to Firestore.
+   * @param content String, text content of the review to be posted to Firestore.
+   */
   postReview(rating: number, content: string) {
     const formattedDate = this.createDate();
     const review: Review = {
@@ -33,16 +45,23 @@ export class MovieDetailsComponent implements OnInit {
     this.reviewService.addReview(review)
   }
 
+  /**
+   * Helper function to create a string for a date from a template in the MM/DD/YYYY format.
+   * @returns String, date in MM/DD/YYYY format.
+   */
   private createDate() {
     const date = new Date();
     const formattedDate = `${date.getUTCMonth() + 1}/${date.getUTCDate()}/${date.getUTCFullYear()}`;
     return formattedDate;
   }
 
+  /**
+   * Helper function to clear user input after they have posted a review. 
+   * Input area for review content returns to placeholder text.
+   */
   private clearUserInput() {
     const reviewTextArea = document.getElementById('reviewTextInput') as HTMLTextAreaElement;
     if (reviewTextArea) {
-      console.log('clearing user input from text area')
       reviewTextArea.value = "";
     }
   }
