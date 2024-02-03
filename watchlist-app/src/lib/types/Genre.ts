8574c90744e71b3d44b223f6522a126666b03b0f
Dale Donnelly <d.ded472@gmail.com>
2024-02-03 16:09:12 -0500

    GenreIds array generated from GenreIdToGenre map

--- watchlist-app/src/lib/types/Genre.ts
+++ watchlist-app/src/lib/types/Genre.ts
@@ -25,5 +25,4 @@ GenreIdToGenre.set(37, 'Western')
 /**
  * A list of all Genre IDs from TMDB.
  */
-export const GenreIds: number[] = 
-[ 28, 12, 16, 35, 80, 99, 18, 10751, 14, 36, 27, 10402, 9648, 10749, 878, 10770, 53, 10752, 37 ]
+export const GenreIds: number[] = Array.from(GenreIdToGenre.keys());
